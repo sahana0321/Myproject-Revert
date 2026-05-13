c372f058b1cef0fbf95a4fcf9cad1e64bd4255de
sahana <sahana@gmail.com>
2026-05-13 17:27:39 +0530

    added-new-features

--- Msg.java
+++ Msg.java
@@ -2,4 +2,6 @@ public class Msg {
 	public static void main(string[] args){
 		int a=10, int b=2;
 		system.out.println("Addition is:" +(a+b));
+		system.out.println("Subtraction is:" +(a-b));
+		system.out.println("Multiplicaion is:" +(a*b));
 	}
